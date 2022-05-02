f038ee86dafc31f30c12b1e9d8e0e661924c3e9b
giruda <qlrqod1778@gmail.com>
2022-05-02 15:14:47 +0900

    컨텐츠 수정

---
+++ jq/slide.js
@@ -0,0 +1,11 @@
+$(window).ready(function () {
+
+    /* 헤더 메인 비주얼 */
+  $("#h-slider .sliders").bxSlider({
+    minSlides: 1,
+    moveSlides: 1,
+    auto: true,
+    pause: 3000,
+    autoControls:true,
+  });
+});
